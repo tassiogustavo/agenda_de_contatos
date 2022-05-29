a0edc0ddff18a1d2f1d5549e010ac05664144161
Tassio Gustavo <tassio-v@outlook.com>
2022-05-29 10:16:28 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/tassio/com/agenda_contatos/MainActivity.kt
@@ -0,0 +1,6 @@
+package tassio.com.agenda_contatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:agenda_contatos/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
